cce79efb6962f41ba057a11457d56016e057a1d0
t74ops <18109551+t74ops@users.noreply.github.com>
2026-08-19 07:03:46 +0900

    wip: load token from .env file

--- exporter.py
+++ exporter.py
@@ -2,8 +2,9 @@ import os
 import requests
 import pprint
 import json
+from dotenv import load_dotenv
 
-
+load_dotenv()
 token = os.environ.get("NATURE_REMO_TOKEN")
 
 url = "https://api.nature.global/1/devices"
@@ -12,12 +13,9 @@ headers = {'Authorization': 'Bearer ' + token}
 res = requests.get(url, headers=headers)
 data = res.json()
 
-print(json.dumps(data))
-
 for device in data:
     device_name = device['name']
     device_temp = device['newest_events']['te']['val']
     print(device_name, device_temp)
 
 
-# pprint.pprint(data)
